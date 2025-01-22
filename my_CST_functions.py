b18e47b38fbe5bb37c42d84071f752c009042bf4
alexrdavidson99 <alex.davidson@warwick.ac.uk>
2025-01-22 11:46:07 +0000

    add a summer to get total electrons per parameter

--- my_CST_functions.py
+++ my_CST_functions.py
@@ -11,11 +11,13 @@ def process_section_data(section_data, section_index):
 def output_data_from_file(file_path):
     # Initialize a list to store data frames for each section
     data_frames = []
+    section_sums = []
 
     # Open the file and read data section by section
     with open(file_path, 'r') as file:
         section_data = []  # To accumulate data for the current section
         section_index = 1
+        current_sum = 0
 
         for line in file:
             if line.startswith('#'):  # Check for section headers
@@ -24,6 +26,7 @@ def output_data_from_file(file_path):
                     data_frames.append((new_section_name, filtered_df))
                     section_data = []  # Reset for the next section
                     section_index += 1
+                    current_sum = 0
                 
                 # Extract section name from comment line
                 section_name = line.strip().replace('#', '', 1).strip()
@@ -35,9 +38,11 @@ def output_data_from_file(file_path):
                     x = float(parts[0])
                     z = float(parts[1])
                     section_data.append((x, z))
+                    current_sum += z
 
         # Process the last section after end of file
         if section_data:
             new_section_name, filtered_df = process_section_data(section_data, section_index)
             data_frames.append((new_section_name, filtered_df))
-    return data_frames
+            section_sums.append(current_sum)
+    return data_frames, section_sums
